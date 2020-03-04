4074a710159f40e13c79ef9d558af428d4e0ce46
Ng Cheng Kiang <ckng@ctl.creative.com>
2020-03-04 22:16:57 +0800

    Made the table narrower

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const AsciiTable = require('ascii-table')
 
-const token = 'YOUR_API_KEY';
+const token = process.env.token || 'YOUR_API_KEY';
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -15,7 +15,7 @@ bot.onText(/\/corona/, async (msg, match) => {
   const coronaSummary = await getCoronaSummary();
 
   await bot.sendMessage(chatId, `<strong>${coronaInfo.title}</strong>\n${coronaInfo.body}\n<code>${coronaSummary}</code>`, { parse_mode: "HTML" });
-  await bot.sendPhoto(chatId, `https://www.gov.sg/${coronaInfo.dorsconImg}`)
+  //await bot.sendPhoto(chatId, `https://www.gov.sg/${coronaInfo.dorsconImg}`)
 });
 
 async function getCoronaInfo() {
@@ -25,7 +25,7 @@ async function getCoronaInfo() {
 
   const title = '';
   const content = $('.feature-intro__content').text().trim()
-  const body = content.substr(content.indexOf('Case Summary'))
+  const body = content.substr(content.indexOf('Case Summary'), content.indexOf('More info on the cases') - content.indexOf('Case Summary'))
 
   const dorsconImg = $('.component img.img-responsive[src*=dorscon]').attr("src")
 
@@ -38,26 +38,38 @@ async function getCoronaSummary() {
   const data = await axios.get('https://spreadsheets.google.com/feeds/list/1lwnfa-GlNRykWBL5y7tWpLxDoCfs8BvzWxFjeOZ1YJk/1/public/values?alt=json')
 
   const table = new AsciiTable()
-  table.setHeading('#', 'Country', 'Cases', 'Deaths')
+  table.setHeading('Country', '😷 / 💀')
 
   const entries = data.data.feed.entry;
-  const updated = data.data.feed.updated['$t']
-
+ 
   const summary = entries
     .sort((a, b) => (b['gsx$confirmedcases']['$t'] || 0) - (a['gsx$confirmedcases']['$t'] || 0))
-    .slice(0, 15).map(entry => {
+    .slice(0, 20).map(entry => {
       return {
-        country: entry['gsx$country']['$t'].replace(' (Diamond Princess)', ''),
+        country: entry['gsx$country']['$t']
+          .replace(' (Diamond Princess)', '')
+          .replace('United States', 'USA')
+          .replace('United Kingdom', 'UK')
+          .replace('South Korea', 'S. Korea')
+          .replace('Singapore', 'S\'pore')
+          .replace('Cruise ship', 'C. Ship')
+          .replace('Hong Kong', 'HK')
+          .replace('Malaysia', 'M\'sia')
+          .replace('Switzerland', 'Switz'),
         cases: entry['gsx$confirmedcases']['$t'] || 0,
-        deaths: entry['gsx$reporteddeaths']['$t'] || 0
+        deaths: entry['gsx$reporteddeaths']['$t'].replace(',', '') || 0
       }
     })
 
   summary.forEach((line, index) => {
-    table.addRow(index + 1, line.country, line.cases, line.deaths)
-    table.setAlign(2, AsciiTable.RIGHT)
-    table.setAlign(3, AsciiTable.RIGHT)
+    table.addRow((index + 1).toString().padStart(2, ' ') + '. ' + line.country, line.cases + ' / ' + line.deaths)
+    table.setAlign(1, AsciiTable.RIGHT)
+    //table.setAlign(2, AsciiTable.RIGHT)
   })
 
+  table.removeBorder();
+  
+  //console.log(updated);
+  //console.log(table.toString() + '\nUpdated: ' + updated);
   return table.toString();
 }
